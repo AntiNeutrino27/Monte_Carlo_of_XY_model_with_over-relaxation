f9cf3654fa1ba9c56d5c30894581112c38e5548c
AntiNeutrino27 <norayrgalikyan@gmail.com>
2026-01-06 01:48:17 +0400

    Added overrelaxation and metroplois sweep methods

--- enviorment.hpp
+++ enviorment.hpp
@@ -3,13 +3,14 @@
 
 #include "./pvector.hpp"
 #include "./particle.hpp"
-#include<vector>
-#include<array>
+#include <vector>
+
 template<typename ntype>
 class XYmodel
 {
+private:
+    int L;
 public:
- int L;
  std::vector<particle<ntype>> parts; // particles
  std::vector<pvector<ntype,3>> J; // couplings for each particle to + direction
     XYmodel(int Lbox): L(Lbox)
@@ -25,9 +26,11 @@ public:
 
     int d3_d1(int ix, int iy, int iz)
     {
+        // for PBC
         ix%=L;
         iy%=L;
         iz%=L;
+
         return ix + L*iy + L*L*iz;
     }
 
@@ -40,7 +43,14 @@ public:
         return {ix, iy, iz};
     }
 
-    pvector<ntype, 2> calc_H(int index){
+    int get_L()
+    {
+        return L;
+    }
+
+    // calculate local field at site index
+    pvector<ntype, 2> calc_H(int index)
+    {  
         auto [ix, iy, iz] = d1_3d(index);
         pvector<ntype,2> si = parts[index].s;
         pvector<ntype,2> H = {0.0, 0.0};
@@ -89,6 +99,51 @@ public:
         return E/2.0; // each bond counted twice
     }
 
+    XYmodel<ntype> make_replica() const
+    {  
+        XYmodel<ntype> replica(L);
+
+        // Copy bonds
+        replica.J = J;
+
+        // Reinitialize spins
+        for (int i = 0; i < N; ++i)
+            replica.parts[i] = particle<ntype>();
+
+        return replica;
+    }
+
+    void or_sweep()
+    {
+        int N = L*L*L;
+        for(int i=0; i<N; i++){
+            pvector<ntype,2> H = calc_H(i);
+            parts[i].ov_rel_step(H);
+            parts[i].store();
+        }
+    }
+
+    void metropolis_sweep(ntype Temp, ntype d_theta_max)
+    {
+        ntype beta = 1.0/Temp;
+        int N = L*L*L;
+        for(int i=0; i<N; i++)
+        {   
+            ntype eno = energy_particle(i);
+            ntype dtheta = rng.ranf()*d_theta_max - d_theta_max/2.0;
+            parts[i].tra_move(dtheta);
+
+            // calculate energy difference
+            ntype enn = energy_particle(i);
+            ntype delu= enn-eno;
+            ntype xi = rng.ranf();
+            if (delu > 0.0 && xi >= exp(-beta*delu))
+            {
+                // reject move
+                parts[i].restore(); 
+            }
+        }
+    }
 };
 
 
