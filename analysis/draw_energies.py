ccc1a654e91aa28aa426354f743c199f1d64af5a
AntiNeutrino27 <norayrgalikyan@gmail.com>
2026-01-26 16:04:02 +0100

    Added parameter parsing from the READMEs

--- analysis/draw_energies.py
+++ analysis/draw_energies.py
@@ -7,19 +7,42 @@ from scipy.signal import savgol_filter
 import os
 
 def calc_temperatures(T_min, T_max, n_T):
+    if n_T == 1:
+        return np.array([T_min])
     return np.array([T_min * math.pow(T_max/T_min, i/(n_T-1.0)) for i in range(n_T)])
 
 
 if __name__ == "__main__":
-    path = "../simulations/simulation1"
-    temps = calc_temperatures(0.265, 0.45, 35)
+    path = "../simulations/simulation11"
+    with open(path + os.sep + "README.txt", encoding='utf-8') as file:
+        rm = file.read()
+        for line in rm.split("\n"):
+            if line.startswith("Number of temperatures:"):
+                n_T = int(line.split()[-1])
+            if line.startswith("Temperature range:"):
+                parts = line.split()
+                T_min = float(parts[-3])
+                T_max = float(parts[-1])
+            if line.startswith("Total steps:"):
+                N = int(line.split()[-1])
+        print(f"n_T={n_T}, T_min={T_min}, T_max={T_max}, N={N}")
+    temps = calc_temperatures(T_min, T_max, n_T)
     for i in range(len(temps)):
         fldr = "temp_" + str(i)
         temp = temps[i]
         energies = np.genfromtxt(f"{path}/{fldr}/energy.dat")
-        smoothed = savgol_filter(energies[:, 1], window_length=20, polyorder=3)
-        plt.plot(energies[:, 0], smoothed, label=f"T={temp:.3f}")
+        # energies = np.genfromtxt(f"{path}/{fldr}/theta.dat") # theta
+        size = energies.shape[0]//100
+        smoothed = savgol_filter(energies[:, 1], window_length=size, polyorder=2) #* 180/np.pi  # convert to degrees
+        plt.plot((energies[:, 0])[size:-size], smoothed[size:-size], label=f"T={temp:.3f}")
+        if i == 0:
+            plt.title("Energy vs Monte Carlo Steps for Different Temperatures")
+            plt.xlabel("Monte Carlo Steps")
+            plt.ylabel("Energy")
 
+    
+    plt.xticks(np.arange(0, 1.1*N, 0.1*N))
+    plt.grid()
     plt.legend()
     plt.show()
         
